9476288260d9c1aac0d1d8c33e3813ec4c724bdf
Jonathan Ternes <ternes.jonathangabriel@gmail.com>
2026-05-08 21:47:10 -0300

    refactor: Organizando os espaços

--- src/main/java/com/jonathan/todolist/config/AdminSeeder.java
+++ src/main/java/com/jonathan/todolist/config/AdminSeeder.java
@@ -8,9 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.time.LocalDateTime;
-import java.util.UUID;
-
 @Configuration
 public class AdminSeeder {
 
